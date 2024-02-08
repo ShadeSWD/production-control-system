b893e14e1cebcc57e8ee7785b4f3864a0525eb45
ShadeSWD <seawolfy60@gmail.com>
2024-02-08 23:46:00 +0300

    full shift model

--- production_tasks/main.py
+++ production_tasks/main.py
@@ -1,14 +1,38 @@
 from fastapi import FastAPI
-from models import SessionLocal, WorkShift
+from models import SessionLocal, WorkShift, Base, engine
+from datetime import date, datetime
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-# Create (Create)
 @app.post("/work_shifts/")
-async def create_work_shift(closing_status: bool):
+async def create_work_shift(closing_status: bool,
+                            shift_assignment: str,
+                            line: str,
+                            shift: int,
+                            brigade: str,
+                            lot_number: int,
+                            lot_date: date,
+                            nomenclature: str,
+                            ekn_code: str,
+                            rc_identifier: str,
+                            open_at: datetime,
+                            closed_at: datetime | None):
     db = SessionLocal()
-    db_item = WorkShift(closing_status=closing_status)
+    db_item = WorkShift(closing_status=closing_status,
+                        shift_assignment=shift_assignment,
+                        line=line,
+                        shift=shift,
+                        brigade=brigade,
+                        lot_number=lot_number,
+                        lot_date=lot_date,
+                        nomenclature=nomenclature,
+                        ekn_code=ekn_code,
+                        rc_identifier=rc_identifier,
+                        open_at=open_at,
+                        closed_at=closed_at)
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
--- production_tasks/models.py
+++ production_tasks/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, create_engine
+from sqlalchemy import Column, Integer, String, Boolean, Date, DateTime, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
@@ -15,13 +15,24 @@ SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_P
                           f"{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Database = declarative_base()
+Base = declarative_base()
 
 
-class WorkShift(Database):
+class WorkShift(Base):
     __tablename__ = "work_shifts"
     id = Column(Integer, primary_key=True, index=True)
-    closing_status = (Column, Boolean)
+    closing_status = Column(Boolean)
+    shift_assignment = Column(String)
+    line = Column(String)
+    shift = Column(Integer)
+    brigade = Column(String)
+    lot_number = Column(Integer, index=True)
+    lot_date = Column(Date, index=True)
+    nomenclature = Column(String)
+    ekn_code = Column(String)
+    rc_identifier = Column(String)
+    open_at = Column(DateTime)
+    closed_at = Column(DateTime, nullable=True)
 
 
-Database.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
