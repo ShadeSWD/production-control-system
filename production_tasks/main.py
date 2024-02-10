51f65cf546eb40c0630d156d1d66429d60f75793
ShadeSWD <seawolfy60@gmail.com>
2024-02-10 10:58:46 +0300

    closed at check

--- production_tasks/main.py
+++ production_tasks/main.py
@@ -1,3 +1,5 @@
+import datetime
+
 import schemas
 from fastapi import FastAPI, HTTPException
 from models import Base, Product, SessionLocal, WorkShift, engine
@@ -20,13 +22,21 @@ async def create_work_shift(work_shift: schemas.WorkShiftCreate):
     )
 
     if existing_work_shift:
-        for key, value in work_shift.dict().items():
-            setattr(existing_work_shift, key, value) if value else None
+        existing_work_shift = update_work_shift(existing_work_shift, work_shift)
         db.commit()
         db.refresh(existing_work_shift)
         return existing_work_shift
 
     db_work_shift = WorkShift(**work_shift.dict())
+    if db_work_shift.closing_status:
+        if (
+            db_work_shift.shift_end
+            > datetime.datetime.now(datetime.UTC)
+            > db_work_shift.shift_start
+        ):
+            db_work_shift.closed_at = datetime.datetime.now(datetime.UTC)
+        else:
+            db_work_shift.closed_at = db_work_shift.shift_end
     db.add(db_work_shift)
     db.commit()
     db.refresh(db_work_shift)
@@ -74,16 +84,31 @@ async def read_work_shift(work_shift_id: int):
 
 
 @app.patch("/work_shifts/{work_shift_id}", response_model=schemas.WorkShift)
-async def update_item(work_shift_id: int, work_shift: schemas.WorkShiftPatch):
+async def patch_work_shift(work_shift_id: int, work_shift: schemas.WorkShiftPatch):
     db = SessionLocal()
     db_work_shift = db.query(WorkShift).filter(WorkShift.id == work_shift_id).first()
 
     if db_work_shift is None:
         raise HTTPException(status_code=404, detail="Shift not found")
 
-    for key, value in work_shift.dict().items():
-        setattr(db_work_shift, key, value) if value else None
+    db_work_shift = update_work_shift(db_work_shift, work_shift)
 
     db.commit()
     db.refresh(db_work_shift)
     return db_work_shift
+
+
+def update_work_shift(work_shift, data):
+    now_closed = work_shift.closing_status
+
+    for key, value in data.dict().items():
+        setattr(work_shift, key, value) if value else None
+    work_shift.closing_status = data.dict()["closing_status"]
+
+    if not now_closed and work_shift.closing_status and not work_shift.closed_at:
+        work_shift.closed_at = datetime.datetime.now(datetime.UTC)
+
+    if not work_shift.closing_status:
+        work_shift.closed_at = None
+
+    return work_shift
