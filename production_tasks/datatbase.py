6d4a36ab4b161716cab72d147824c2ee1c88d07e
ShadeSWD <seawolfy60@gmail.com>
2024-02-11 16:03:12 +0300

    fix env missing

--- production_tasks/datatbase.py
+++ production_tasks/datatbase.py
@@ -5,17 +5,27 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / ".env")
 
-# Database configurations
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:"
-    f"{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-)
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+try:
+    # Database configurations
+    SQLALCHEMY_DATABASE_URL = (
+        f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:"
+        f"{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    )
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+except ValueError:
+    print("Env error, working with test database!")
+    TEST_SQLALCHEMY_DATABASE_URL = "sqlite://"
+    engine = create_engine(
+        TEST_SQLALCHEMY_DATABASE_URL,
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+    )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
